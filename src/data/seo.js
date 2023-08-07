df16eae15a22a8683f8727d696b45d97c706fa44
Ayush0202 <ayush.skg@gmail.com>
2023-08-07 12:59:47 +0530

    first commit

---
+++ src/data/seo.js
@@ -0,0 +1,31 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a full stack web developer. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: [],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a full stack web developer. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Ayush", "Ayush Kumar Gupta"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Ayush", "Ayush Kumar Gupta"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Ayush", "Ayush Kumar Gupta"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,117 @@
+const INFO = {
+	main: {
+		title: "Ayush Kumar Gupta",
+		name: "Ayush Kumar Gupta",
+		email: "ayush.skg@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/Ayush0202",
+		linkedin: "https://www.linkedin.com/in/ayushkumargupta02/",
+		instagram: "https://instagram.com/ayush0202",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+		leetcode: "https://leetcode.com/Ayush0202/",
+		geeksforgeeks: "https://auth.geeksforgeeks.org/user/ayush0202/?utm_source=geeksforgeeks&utm_medium=my_profile&utm_campaign=auth_user",
+		resume: "https://drive.google.com/file/d/1m6H0pQZOd442BNO24JAjV_Npzmk8f8qm/view?usp=sharing"
+	},
+
+	homepage: {
+		title: "👋 Hi, I am Ayush",
+		description: (
+			<div>
+				I am a <u>Full-Stack Web Developer</u> living in Delhi, India. I am currently doing my Bachelors of Technology (B. Tech) from <a style={{color: "blue", textDecoration:"none"}} href={'http://dtu.ac.in/'}>Delhi Technological University</a> (Formerly Delhi College of Engineering) majoring in '<strong>Electrical Engineering</strong>' with a minor in '<strong>Computer Engineering</strong>'.
+				<br />
+				<br />
+				I am currently learning <strong>Data Structures and Algorithms</strong> along with new <strong>Web Technologies</strong> and open to any <u>internship</u> opportunity that come my way
+				<br />
+				<br />
+				You can find my resume <a style={{color: "blue", textDecoration: "none"}} href={'https://drive.google.com/file/d/1m6H0pQZOd442BNO24JAjV_Npzmk8f8qm/view'}>here</a> and further information below
+
+			</div>
+		),
+	},
+
+	about: {
+		title: "I’m Ayush Kumar Gupta 🥷",
+		description: (
+			<div>
+				I am a <u>Full Stack Web Developer</u> and currently live in <strong>Delhi, India</strong>
+				<br />
+				<br />
+				I am currently in my final year of undergrad at <a href={"http://dtu.ac.in/"} style={{color:"blue", textDecoration:"none"}}>Delhi Technological University</a> where I'm majoring in <strong>Electrical Engineering</strong> with a minor in <strong>Computer Science</strong>
+				<br />
+				<br />
+				I am passionate about <strong>Web Technologies</strong> and constantly try to learn new things in it and make myself better. I am also constantly exploring other fields as well such as <strong>Data Science and Machine Learning</strong>
+				<br/>
+				<br/>
+				I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I try to write high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer
+				<br/>
+				<br/>
+				I also love solving <strong>algorithmic</strong> based problems and have solved a combine of <strong>1100+</strong> problems on <a href={"https://leetcode.com/Ayush0202/"} style={{color:"blue", textDecoration:"none"}}>Leetcode</a> and <a href={"https://auth.geeksforgeeks.org/user/ayush0202/?utm_source=geeksforgeeks&utm_medium=my_profile&utm_campaign=auth_user"} style={{color:"blue", textDecoration:"none"}}>GeeksForGeeks</a>
+			</div>
+		)
+
+	},
+
+
+	projects: [
+		{
+			title: "Codeshare",
+			description:
+				"A MERN Stack Application that enables easy sharing of codes",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Ayush0202/Codeshare-2.0/",
+		},
+
+		{
+			title: "Wordsync",
+			description:
+				"A Real-time document collaboration platform ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Ayush0202/Wordsync",
+		},
+
+		{
+			title: "NutriGo",
+			description:
+				"React application that provides nutritional data of food items",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Ayush0202/NutriGo",
+		},
+
+		{
+			title: "Dilli Darbar",
+			description:
+				"Simple News Portal Application",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/Ayush0202/Dilli-Darbar",
+		},
+
+		{
+			title: "Chit Chat",
+			description:
+				"Realtime Chat App using Node.js",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/Ayush0202/RealTime-Chat-Application",
+		},
+
+		{
+			title: "Keeper App",
+			description:
+				"Google Keep Clone using React.js",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Ayush0202/KeeperApp-Google-Keep-Clone",
+		},
+	],
+};
+
+export default INFO;
